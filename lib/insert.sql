e33b5af7e177d39eda9d0f8bfb173a248edb6ecf
Nicole Dow <nikki.dow71@gmail.com>
2019-08-10 22:27:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "F", "brown", "mean", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "F", "brown", "mean", 1);
